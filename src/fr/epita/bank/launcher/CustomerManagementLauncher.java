0853263521f17b04fac56a29e6bf5d5bfca95849
asish98754 <ocrack173@gmail.com>
2025-02-11 11:44:35 +0100

    implement application flow

---
+++ src/fr/epita/bank/launcher/CustomerManagementLauncher.java
@@ -0,0 +1,40 @@
+package fr.epita.bank.launcher;
+
+import fr.epita.bank.datamodel.Customer;
+import fr.epita.bank.datamodel.SavingsAccount;
+import fr.epita.bank.service.ApplicationActivitiesService;
+
+import java.util.Scanner;
+
+public class CustomerManagementLauncher {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to this bank application");
+        System.out.println("What would you like to do?");
+        System.out.println("1. create customer");
+        System.out.println("2. withdraw amount from an account");
+        System.out.println("3. quit the program");
+        System.out.println("type your option (1 or 2 or 3)");
+        Scanner scanner = new Scanner(System.in);
+        String option = scanner.nextLine();
+        if ("1".equals(option)){
+            Customer customer = ApplicationActivitiesService.createCustomerActivity(scanner);
+
+            SavingsAccount savingsAccount = ApplicationActivitiesService.createSavingAccountActivity(500.0, customer, 0.025);
+
+
+
+
+
+
+
+        } else if ("2".equals(option)) {
+
+        } else if ("3".equals(option)) {
+
+        }else {
+            System.out.println("unrecognized option");
+        }
+
+    }
+}
--- src/fr/epita/bank/launcher/Main.java
+++ src/fr/epita/bank/launcher/Main.java
@@ -2,24 +2,46 @@ package fr.epita.bank.launcher;
 
 import fr.epita.bank.datamodel.Customer;
 import fr.epita.bank.datamodel.SavingsAccount;
+import fr.epita.bank.service.AccountService;
+
+import java.util.Scanner;
+
+import static fr.epita.bank.service.ApplicationActivitiesService.*;
+
 
 public class Main {
 
-
-
     public static void main(String[] args) {
-        // 1. create a customer named quentin
-        // 2. assign to this customer a savings account initialized with
+        // 1.create a customer named customer
+        // 2. assign to this customer a savings account initialized with 500€
         // 3. calculate the yearly gain linked to the interest rate
         // 4. add the gain to the savings balance
-        // 5. the customer wants to withdraw 300€ then 500€, if the bal
-        // 6. insufficient, then it should print a warning
+        // 5. the customer wants to withdraw 300€ then 500€, if the balance is
+        // insufficient, then it should print a warning
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to this bank application");
 
-        Customer customer = new Customer("quentin", "paris");
-        SavingsAccount savingsAccount = new SavingsAccount(500.0, customer);
-        savingsAccount.setBalance(-100000);
-        double gain = savingsAccount.balance * savingsAccount.interestRate;
+
+        Customer customer = createCustomerActivity(scanner);
+
+        double interestRate = 0.025;
+        double initialBalance = 500.0;
+
+
+        SavingsAccount savingsAccount = createSavingAccountActivity(initialBalance, customer, interestRate);
+
+
+        AccountService.creditInterest(savingsAccount);
+
+        //5.
+
+        // trigger withdraw activity
+        withdrawActivity(scanner, savingsAccount);
 
 
     }
+
+
+
+
 }
